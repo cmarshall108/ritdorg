e54a52e6111d9419c67ae1617339a58ed8408ee2
Caleb Marshall <caleb.marshall108@gmail.com>
2026-03-11 18:15:48 -0400

    update python path in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,8 @@ pip install -r requirements.txt --break-system-packages
 pip install uvicorn asgiref --break-system-packages
 
 echo "Starting server on 0.0.0.0:80 with uvicorn..."
-sudo $(which python) -c "
+PYTHON_BIN=$(which python3 || which python)
+sudo "$PYTHON_BIN" -c "
 from asgiref.wsgi import WsgiToAsgi
 from app import app
 import uvicorn
